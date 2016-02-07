73fa88976e34fb84fcf701e418a19577c199f2c9
Nicolas Frankel <nicolas@frankel.ch>
2016-02-07 12:23:02 +0100

    Automatically register converter beans

--- src/main/java/ch/frankel/boot/xstream/XStreamAutoConfiguration.java
+++ src/main/java/ch/frankel/boot/xstream/XStreamAutoConfiguration.java
@@ -1,9 +1,12 @@
 package ch.frankel.boot.xstream;
 
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.converters.Converter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collection;
 
 @Configuration
 public class XStreamAutoConfiguration {
@@ -12,5 +15,11 @@ public class XStreamAutoConfiguration {
     @ConditionalOnMissingBean(XStream.class)
     public XStream xStream() {
         return new XStream();
+    }
+
+    @Bean
+    public Collection<Converter> converters(XStream xstream, Collection<Converter> converters) {
+        converters.forEach(xstream::registerConverter);
+        return converters;
     }
 }
