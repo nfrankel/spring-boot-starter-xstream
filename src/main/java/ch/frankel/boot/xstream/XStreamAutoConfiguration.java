b028b383f21d74adba80c0b260aa1371f030795d
Nicolas Frankel <nicolas@frankel.ch>
2016-02-07 12:20:07 +0100

    Instantiate default XStream bean

--- src/main/java/ch/frankel/boot/xstream/XStreamAutoConfiguration.java
+++ src/main/java/ch/frankel/boot/xstream/XStreamAutoConfiguration.java
@@ -1,6 +1,16 @@
 package ch.frankel.boot.xstream;
 
+import com.thoughtworks.xstream.XStream;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class XStreamAutoConfiguration {}
+public class XStreamAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean(XStream.class)
+    public XStream xStream() {
+        return new XStream();
+    }
+}
