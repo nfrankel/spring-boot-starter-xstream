7b9e06de3e5d27ae1bc807738671f4dae07d1691
Nicolas Frankel <nicolas@frankel.ch>
2016-05-25 19:11:30 +0200

    Automatically register underlying driver

--- src/main/java/ch/frankel/boot/xstream/XStreamAutoConfiguration.java
+++ src/main/java/ch/frankel/boot/xstream/XStreamAutoConfiguration.java
@@ -2,6 +2,8 @@ package ch.frankel.boot.xstream;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.converters.Converter;
+import com.thoughtworks.xstream.io.HierarchicalStreamDriver;
+import com.thoughtworks.xstream.io.xml.XppDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -9,14 +11,15 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 import java.util.Collection;
+import java.util.Optional;
 
 @Configuration
 public class XStreamAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean(XStream.class)
-    public XStream xStream() {
-        return new XStream();
+    public XStream xStream(Optional<HierarchicalStreamDriver> driver) {
+        return new XStream(driver.orElse(new XppDriver()));
     }
 
     @Configuration
